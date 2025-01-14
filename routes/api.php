8a11925d8634049c7070327dd1a515145abd13e6
juliano Hogberg <julianokew@hotmail.com>
2025-01-14 09:52:04 +0000

    new file:   app/Http/Controllers/AuthController.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post(uri: '/register',action: [AuthController::class, 'register']);
+Route::post(uri: '/login',action: [AuthController::class, 'login']);
+
+
+// get all posts no need to authenticate
