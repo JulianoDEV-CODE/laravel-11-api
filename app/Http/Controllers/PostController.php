0f356270d41e793893eb989edb98d583f677b075
juliano Hogberg <julianokew@hotmail.com>
2025-01-16 14:03:10 +0000

    Add Post and Comment functionality with API endpoints

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Post;
+use App\Http\Resources\SinglePostResource;
+
+
+class PostController extends Controller
+{
+    public function addNewPost(Request $request){
+        $validated = Validator::make($request->all(),[
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(),403);
+        }
+
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->user_id = \Illuminate\Support\Facades\Auth::user()->id;
+            $post->save();
+
+             //return
+             return response()->json([
+                'message' => 'Post added successfully',
+            ],200);
+
+        } catch (\Exception $th) {
+            return response()->json(['error' => $th->getMessage()],403);
+        }
+    }
+
+    //edit a post
+    public function editPost(Request $request){
+        $validated = Validator::make($request->all(),[
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'post_id' => 'required|integer',
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(),403);
+        }
+
+        try {
+            $post_data = Post::find($request->post_id);
+
+           $updatePost = $post_data->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+             //return
+             return response()->json([
+                'message' => 'Post updated successfully',
+                'updated_post' => $updatePost,
+            ],200);
+
+        } catch (\Exception $th) {
+            return response()->json(['error' => $th->getMessage()],403);
+        }
+    }
+
+    //edit a post approach 2
+    public function editPost2(Request $request,$post_id){
+        $validated = Validator::make($request->all(),[
+            'title' => 'required|string',
+            'content' => 'required|string',
+
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(),403);
+        }
+
+        try {
+            $post_data = Post::find($post_id);
+
+           $updatePost = $post_data->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+             //return
+             return response()->json([
+                'message' => 'Post updated successfully ok',
+                'updated_post' => $updatePost,
+            ],200);
+
+        } catch (\Exception $th) {
+            return response()->json(['error' => $th->getMessage()],403);
+        }
+    }
+
+    //retrieve all posts
+    public function getAllPosts(){
+        try {
+            $posts = Post::all();
+            return response()->json([
+                'posts' => $posts
+            ],200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()],403);
+        }
+    }
+
+
+    //fetch single post
+    public function getPost($post_id){
+        try {
+          $post = Post::where('id',$post_id)->first();
+
+            return response()->json([
+                'post' => $post
+
+            ],200);
+        } catch (\Exception $th) {
+                        return response()->json(['error' => $th->getMessage()],403);
+        }
+    }
+
+    public function deletePost(Request $request, $post_id){
+        try {
+            $post = Post::find($post_id);
+            $post->delete();
+            return response()->json([
+                'message' => 'post deleted successfully ok'
+            ],200);
+        } catch (\Exception $th) {
+            return response()->json(['error' => $th->getMessage()],403);
+
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,34 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//no need to authenticate
 Route::post(uri: '/register',action: [AuthController::class, 'register']);
+
 Route::post(uri: '/login',action: [AuthController::class, 'login']);
 
+Route::get('/all/posts',[PostController::class,'getAllPosts']);
+Route::get('/single/post/{post_id}',[PostController::class,'getPost']);
 
 // get all posts no need to authenticate
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[AuthController::class, 'logout']);
+
+   //blog api
+
+Route::post('/add/post', [PostController::class,'addNewPost']);
+
+//edit approach 1
+Route::post('/edit/post', [PostController::class,'editPost']);
+// delete post
+
+Route::post('/delete/post/{post_id}', [PostController::class,'deletePost']);
+
+Route::post('/edit/post/{post_id}', [PostController::class,'editPost2']);
+});
+// Route::post('/add/post', [PostController::class,'addNewPost'
